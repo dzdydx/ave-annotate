5427c0bcc10f1db0a48cd1b06ec4f4d63bc2dde0
dzdydx <sirly.liu@qq.com>
2024-10-30 23:24:23 +0800

    add home page and global layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,27 @@
-import { Link, Outlet } from 'umi';
-import styles from './index.less';
+import { Outlet } from "umi";
+import "./index.less";
+import { Layout, Progress } from "antd";
 
-export default function Layout() {
+const { Content, Footer } = Layout;
+
+export default function BasicLayout() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
+    <div>
+      <Progress
+        type="line"
+        percent={0}
+        format={(percent) => `${percent} / 97260`}
+        percentPosition={{ align: "center", type: "inner" }}
+        size={["100%", 25]}
+      />
+      <Layout style={{ height: "calc(100vh - 25px)" }}>
+        <Content>
+          <Outlet />
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          dzdydx ©{new Date().getFullYear()}
+        </Footer>
+      </Layout>
     </div>
   );
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Typography, Form, Input, Button } from "antd";
+import { history } from "umi";
+
+const { Title, Paragraph } = Typography;
+
+const HomePage: React.FC = () => {
+  const onFinish = (values: { name: string }) => {
+    history.push({
+      pathname: "/annotate",
+      search: `?name=${values.name}`,
+    });
+  };
+
+  return (
+    <div
+      style={{
+        height: "100%",
+        padding: "48px 48px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Title level={1}>Annotating AVE-PM</Title>
+      <Paragraph>Please enter the ID of current annotator:</Paragraph>
+      <Form layout="inline" onFinish={onFinish}>
+        <Form.Item name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default HomePage;
